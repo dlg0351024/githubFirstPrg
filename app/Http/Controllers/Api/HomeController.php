f30346c1ff925e42e8e8232cb62da68542d94fdd
slg0922 <shi787944035@foxmail.com>
2019-04-30 10:06:08 +0800

    获取数据方法提交

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -4,5 +4,11 @@ class home{
 	public funtion index(){
 		return view("");
 	}
+
+	//index处理方式
+	public funtion getDataList(){
+		$retData=array();
+		return response(ReturnCode::success,$retData);
+	}
 }
 ?>
