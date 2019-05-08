57bde6eb2f6e998da878552d484e2591c4bce0e5
slg0922 <shi787944035@foxmail.com>
2019-05-08 12:14:04 +0800

    测试

--- app/Http/Controllers/Api/AdminUserController.php
+++ app/Http/Controllers/Api/AdminUserController.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Http\Controller\Api;
 
+
 class AdminUserController extends Controller{
      
      public function getUserInfo(){
      	
+     	Log::info(11);
+     	 
      	$userInfo=User::find($id);
      	return $userInfo();
      }
